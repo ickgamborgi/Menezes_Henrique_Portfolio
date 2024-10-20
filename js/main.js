85cae9ed98e03c6c5fbae172204aa2b3cc8a6d30
Henrique Gamborgi <henriquegamborgi@gmail.com>
2024-10-20 06:35:41 -0400

    Update Design Styling

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 console.log("javascript file is linked");
+
+// VARIABLES
+
+// Header
+const toggleButton = document.querySelector("#burger-button")
+const navbarLinks = document.querySelector(".links-header")
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
