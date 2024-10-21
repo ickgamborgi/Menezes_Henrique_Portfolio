0b118d206af6a711daa81cb75bee2a5f16b1a797
Henrique Gamborgi <henriquegamborgi@gmail.com>
2024-10-20 20:37:18 -0400

    Update homepage design

--- js/main.js
+++ js/main.js
@@ -11,3 +11,30 @@ toggleButton.addEventListener('click', () => {
 
 // Video Player
 const player = new Plyr("#demoreel");
+
+// Testimonials
+let testimonialIndex = 0;
+const testimonialCards = document.querySelectorAll(".testimonial-card");
+
+function showTestimonial(index) {
+  testimonialCards.forEach((card, i) => {
+    if (i === testimonialIndex) {
+      card.classList.remove("visible"); // Remove the visible class from the default testimonials
+    }
+  });
+
+  testimonialIndex = (index + testimonialCards.length) % testimonialCards.length;
+
+  const newCard = testimonialCards[testimonialIndex];
+  newCard.classList.add("visible"); // Add the visible class to selected testimonial
+}
+
+showTestimonial(testimonialIndex); // Show the first testimonial as default
+
+document.querySelector("#test-next-btn").addEventListener("click", () => {
+  showTestimonial(testimonialIndex + 1);
+});
+
+document.querySelector("#test-prev-btn").addEventListener("click", () => {
+  showTestimonial(testimonialIndex - 1);
+});
