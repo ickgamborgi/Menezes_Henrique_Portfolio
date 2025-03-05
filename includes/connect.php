141e0d369c1b63f7c1a94b46d833e82b5e033eaa
Henrique Gamborgi <henriquegamborgi@gmail.com>
2025-03-05 17:46:55 -0500

    Update Hosting

--- includes/connect.php
+++ includes/connect.php
@@ -1,9 +1,24 @@
 <?php
-$dsn = "mysql:host=localhost;dbname=Menezes_Henrique_Portfolio;charset=utf8mb4";
+
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
+    // Local connection
+    $dsn = "mysql:host=localhost;dbname=Menezes_Henrique_Portfolio;charset=utf8mb4";
+    $username = 'root'; // your local username if different
+    $password = 'root'; // your local password
+} else {
+    // Live server connection (adjust these values accordingly)
+    $dsn = "mysql:host=localhost;dbname=mmlf6971_portfolio;charset=utf8mb4";
+    $username = 'mmlf6971_henriquegamborgi';
+    $password = 'lY3u9u*Sig]N';
+}
+
 try {
-$connect = new PDO($dsn, 'root', 'root');
+    // Create a new PDO instance
+    $connect = new PDO($dsn, $username, $password);
+    // Set the PDO error mode to exception
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
-  error_log($e->getMessage());
-  exit('unable to connect');
+    error_log($e->getMessage());
+    exit('Unable to connect');
 }
 ?>
